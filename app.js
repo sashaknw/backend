9739628adede6d896743dd349bc162e89ac4078e
sashaknw <kaniewa.aleksandra@gmail.com>
2025-02-18 13:31:32 +0000

    db running

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const morgan = require("morgan");
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 5005;
 
+server.use(cors());
 server.use(middlewares);
 server.use(morgan("dev"));
 server.use(router);
